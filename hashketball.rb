f9c91694cbe27d84b4b65725e366f942d525ef54
placid-tier-4550 <james.ardery@gmail.com>
2020-09-29 14:23:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,7 @@ def game_hash
          :assists => 1,
          :steals => 2,
          :blocks => 7,
-         :slam_dunks => },
+         :slam_dunks => 2},
         {:player_name => "Bismack Biyombo",
          :number => 0,
          :shoe => 16,
@@ -73,7 +73,7 @@ def game_hash
          :assists => 7,
          :steals => 22,
          :blocks => 15,
-         :slam_dunks => },
+         :slam_dunks => 10},
         {:player_name => "DeSagna Diop",
          :number => 2,
          :shoe => 14,
@@ -82,7 +82,7 @@ def game_hash
          :assists => 12,
          :steals => 4,
          :blocks => 5,
-         :slam_dunks => },
+         :slam_dunks => 5},
         {:player_name => "Ben Gordon",
          :number => 8,
          :shoe => 15,
@@ -91,7 +91,7 @@ def game_hash
          :assists => 2,
          :steals => 1,
          :blocks => 1,
-         :slam_dunks => },
+         :slam_dunks => 0},
         {:player_name => "Kemba Walker",
          :number => 33,
          :shoe => 15,
@@ -100,7 +100,7 @@ def game_hash
          :assists => 12,
          :steals => 7,
          :blocks => 5,
-         :slam_dunks => }
+         :slam_dunks => 12}
        ]}
   }
 end
