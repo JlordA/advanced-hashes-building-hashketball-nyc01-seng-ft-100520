78d02e754d423fa3e8d4c640f1ce9341be67c8aa
placid-tier-4550 <james.ardery@gmail.com>
2020-09-29 12:57:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
 # Write your code here!
 def game_hash
-  hashketball = {:home => {:team_name => Brooklyn Nets}, :away => {:team_name => Charlotte Hornets}
+  hashketball = {:home => {:team_name => Brooklyn Nets}, :away => {:team_name => Charlotte Hornets}}
 end
