6581f578ed1184a1d360160f2f6d3252a1885df9
placid-tier-4550 <james.ardery@gmail.com>
2020-09-29 14:02:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,7 +13,7 @@ def game_hash
         :assists => "12",
         :steals => "3",
         :blocks => "1",
-        :slam_dunks},
+        :slam_dunks => "1"},
         {:player_name => "Reggie Evans",
         :number => "30",
         :shoe => "14",
@@ -22,7 +22,7 @@ def game_hash
         :assists => "12",
         :steals => "12",
         :blocks => "12",
-        :slam_dunks},
+        :slam_dunks => "7"},
         {:player_name => "Brook Lopez",
         :number => "11",
         :shoe => "17",
@@ -31,7 +31,7 @@ def game_hash
         :assists => "10",
         :steals => "3",
         :blocks => "1",
-        :slam_dunks},
+        :slam_dunks => "15"},
         {:player_name => "Mason Plumlee",
         :number => "1",
         :shoe => "19",
@@ -40,7 +40,7 @@ def game_hash
         :assists => "6",
         :steals => "3",
         :blocks => "8",
-        :slam_dunks},
+        :slam_dunks => "5"},
         {:player_name => "Jason Terry",
         :number => "31",
         :shoe => "15",
@@ -49,7 +49,7 @@ def game_hash
         :assists => "2",
         :steals => "4",
         :blocks => "11",
-        :slam_dunks}
+        :slam_dunks => "1"}
      ]
      },
     :away =>
